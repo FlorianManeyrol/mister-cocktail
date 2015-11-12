94d9c5a8079da752a5d129278cdcd64e046c2132
Florian Maneyrol <florianmaneyrol@gmail.com>
2015-11-12 14:08:25 +0100

    cocktails controller et routes done

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,44 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update
+    redirect_to cocktail
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.delete
+    redirect_to cocktail
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktails).permit(:name)
+  end
+end
